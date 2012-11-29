625fc915660f361ee242acc502da41baedb7eb26
Stuart Knightley <stuart@stuartk.com>
2012-11-28 16:32:43 -0800

    Correct Montage install instructions in app template

--- templates/app.js
+++ templates/app.js
@@ -60,15 +60,8 @@ exports.Template = Object.create(TemplateBase, {
             TemplateBase.finish.call(this);
 
             // TODO: Do something automatically
-            // This is nowhere near the most user friendly way of adding Montage
-            // to the user's app. We could init a Git repo and submodule it
-            // for them, but that might be a bit presumptuous. To think about.
-            console.log("* Clone Montage to your app:");
-            console.log("git clone git@github.com:Motorola-Mobility/montage.git "+ this.variables.name +"/node_modules/montage");
-            console.log("* or add it as a submodule:");
-            console.log("cd " + this.variables.name);
-            console.log("git init");
-            console.log("git submodule add git@github.com:Motorola-Mobility/montage.git node_modules/montage");
+            console.log("Inside the '" + this.variables.name + "/' directory run:");
+            console.log("npm install");
             console.log();
         }
     }
