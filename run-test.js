c47d9dae4caf7a44569f82978dbebb2352d733df
François Frisch <francoisfrisch@gmail.com>
2012-12-31 18:25:39 -0800

    Add promise support to jasmine.

--- run-test.js
+++ run-test.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var jasmine = require('jasmine-node');
 var sys = require('sys');
+var Q = require("q");
 
 //for(var key in jasmine) {
 //  global[key] = jasmine[key];
@@ -15,6 +18,33 @@ process.argv.forEach(function(arg){
           case '--verbose': isVerbose = true; break;
       }
 });
+
+/**
+ * from https://github.com/kriskowal/q-io/blob/master/spec/lib/jasmine-promise.js
+ */
+jasmine.Block.prototype.execute = function (onComplete) {
+    var spec = this.spec;
+    try {
+        var result = this.func.call(spec, onComplete);
+
+        // It seems Jasmine likes to return the suite if you pass it anything.
+        // So make sure it's a promise first.
+        if (result && typeof result.then === "function") {
+            Q.timeout(result, 500).then(function () {
+                onComplete();
+            }, function (error) {
+                spec.fail(error);
+                onComplete();
+            });
+        } else if (this.func.length === 0) {
+            onComplete();
+        }
+    } catch (error) {
+        spec.fail(error);
+        onComplete();
+    }
+};
+
 
 jasmine.executeSpecsInFolder({
     "specFolder": __dirname+ "/test",
