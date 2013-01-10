79a96ed79e42c87ebee3b98a60ba548396a1e0ac
François Frisch <francoisfrisch@gmail.com>
2013-01-10 13:33:00 -0800

    Add npm install step to app template.

--- templates/app.js
+++ templates/app.js
@@ -1,36 +1,8 @@
-/* <copyright>
-Copyright (c) 2012, Motorola Mobility LLC.
-All Rights Reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of Motorola Mobility LLC nor the names of its
-  contributors may be used to endorse or promote products derived from this
-  software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-</copyright> */
-
 var TemplateBase = require("../lib/template-base.js").TemplateBase;
+var path = require('path');
 var fs = require('fs');
+var npm = require("npm");
+var Q = require('q');
 
 exports.Template = Object.create(TemplateBase, {
 
@@ -62,18 +34,24 @@ exports.Template = Object.create(TemplateBase, {
     },
 
     finish: {
-        value: function() {
+        value: function(destination) {
+            var self = this;
             return TemplateBase.finish.call(this).then(function(result) {
-                // TODO: Do something automatically
-                // This is nowhere near the most user friendly way of adding Montage
-                // to the user's app. We could init a Git repo and submodule it
-                // for them, but that might be a bit presumptuous. To think about.
-                console.log("* Install Montage from npm:");
-                console.log("npm install .");
-                console.log();
-                return result;
+                var config = {
+                    prefix : path.join(destination, self.options.name)
+                };
+                return self.installDependencies(config);
             });
 
+        }
+    },
+
+    installDependencies: {
+        value: function (config) {
+            return Q.ninvoke(npm, "load", (config || null))
+                .then(function (loadedNpm) {
+                    return Q.ninvoke(loadedNpm.commands, "install");
+                });
         }
     }
 
