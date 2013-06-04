db45a6814850ec3ef67a606ad435db0a8dc3b4a1
António Afonso <antonio.afonso@gmail.com>
2013-06-03 18:12:53 -0700

    Update templates to use specialize()

--- client/ui/main.reel/main.js
+++ client/ui/main.reel/main.js
@@ -3,15 +3,14 @@
     @requires montage
     @requires montage/ui/component
 */
-var Montage = require("montage").Montage,
-    Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component;
 
 /**
     Description TODO
     @class module:"ui/main.reel".Main
     @extends module:ui/component.Component
 */
-exports.Main = Montage.create(Component, /** @lends module:"ui/main.reel".Main# */ {
+exports.Main = Component.specialize(/** @lends module:"ui/main.reel".Main# */ {
 
     thing: {
         value: "World"
