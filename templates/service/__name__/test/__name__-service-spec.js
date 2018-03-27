4ae36c9e31cbd286306f592aad34aed9fa2cfd91
Harold Thetiot <hthetiot@gmail.com>
2018-03-27 12:36:46 -0700

    add unit test for template service

---
+++ templates/service/__name__/test/__name__-service-spec.js
@@ -0,0 +1,36 @@
+var request = require('supertest');
+describe('loading express', function () {
+  var server;
+  beforeEach(function () {
+    server = require('./../main');
+  });
+  afterEach(function () {
+    server.close();
+  });
+
+  it('responds to /', function testSlash(done) { 
+    request(server)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('responds to /api/fetchData', function testFetchData(done) {
+
+    // TODO query
+
+    request(server)
+      .get('/api/fetchData')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+          assert(response.body.message, 'Hello World');
+          done();
+      });
+  });
+    
+  it('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+});
